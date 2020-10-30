bcedf3972dfac63175583ca9083145b9bff7a9da
Prachika <prachikapal@gmail.com>
2020-10-30 17:35:59 +0530

    last commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./index.css";
 import Keys from "./Keys";
-import Screen from "./Screen";
+import Screen from "./screen/Screen";
 
 class App extends React.Component {
 
